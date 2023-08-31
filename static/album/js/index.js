93e64afa70cad07c99db51dbffeeaf9be51930d8
JotonW <99779361+JotonW@users.noreply.github.com>
2023-08-31 19:46:48 +0800

    Delete static/album directory

--- static/album/js/index.js
+++
@@ -1,9 +0,0 @@
-var myfile = document.querySelector('#chooseimg');
-var fxfile = document.querySelector('.fxfile');
-var uploadbtn = document.querySelector('.uploadbtn');
-myfile.onchange = function(){
-  if(this.files){
-      fxfile.style.opacity = 1;
-      uploadbtn.style.visibility = 'visible';
-  }
-}
