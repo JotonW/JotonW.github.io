40049946ca6a0be309e38c75ad13d6749f9d61df
JotonW <99779361+JotonW@users.noreply.github.com>
2023-08-31 22:40:46 +0800

    Delete templates/Joton directory

--- templates/Joton/Joton/解码器.py
+++
@@ -1,9 +0,0 @@
-import qrdecode
-import ybc_box as box
-def decode(filename):
-   res = qrdecode.decode(filename)
-   return res
-filename = box.fileopenbox()
-if filename != None:
-   url = decode(filename)
-   print(url)
